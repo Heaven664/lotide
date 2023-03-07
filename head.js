fb0ec60d091584379aaeef27b98767384a5fe0e7
Omar Hamid <omarhamid664@gmail.com>
2023-03-06 20:47:07 -0700

    implemented head and tail funtions

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -7,8 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 10);
+const head = function(arr) {
+  return arr[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), undefined);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello"]), "Hello");
