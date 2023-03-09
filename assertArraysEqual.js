f7abbfdd21b7d68e4b1a8e9007825ad8881cb9d4
Omar Hamid <omarhamid664@gmail.com>
2023-03-08 20:55:51 -0700

    assertObjectsEqual function implemented

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,11 @@
 const assertArraysEqual = function(arr1, arr2) {
   if ((!arr1 || !arr2) || arr1.length !== arr2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} === ${arr2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
     return;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} === ${arr2}`);
+      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
       return;
     }
   }
