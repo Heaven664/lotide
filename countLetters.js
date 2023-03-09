1082dd307e92f616537ee8d676819a0b5e1c9ee6
Omar Hamid <omarhamid664@gmail.com>
2023-03-08 19:21:14 -0700

    implemented 'countLetters' function

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(str) {
+  str = str.split(" ").join('');
+  const result = {};
+  for (let letter of str) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+assertEqual(countLetters("lighthouse in the house")['h'], 4);
+assertEqual(countLetters("lighthouse in the house")['t'], 2);
