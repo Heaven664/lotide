94b14abc10c037ae865f3dc7e38daecd3bb56e45
Omar Hamid <omarhamid664@gmail.com>
2023-03-13 17:53:37 -0600

    refactored middle.js by exporting functions and separated tests to different file

--- middle.js
+++ middle.js
@@ -1,16 +1,4 @@
-const assertArraysEqual = function(arr1, arr2){
-  if (arr1.length !== arr2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} === ${arr2}`);
-    return 
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} === ${arr2}`)
-      return
-    }
-  }
-  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   const result = []
@@ -26,7 +14,4 @@ const middle = function(array) {
   return result;
 }
 
-
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2]), []);
+module.exports = middle;
