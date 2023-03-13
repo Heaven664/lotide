74505937ce39da54c97612992d0b24565048ce58
Omar Hamid <omarhamid664@gmail.com>
2023-03-12 22:54:27 -0600

    eqArrays now compares nested arrays of any depth

--- eqArrays.js
+++ eqArrays.js
@@ -10,13 +10,25 @@ const eqArrays = function(arr1, arr2){
   if (arr1.length !== arr2.length) {
     return false
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
+
+  for (let i in arr1) {
+    if (Array.isArray(arr1[i])) {
+      if(!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
   }
   return true
 }
+
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
