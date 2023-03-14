01a8198929473eb7bf12061a8b1bc082392971e5
Omar Hamid <omarhamid664@gmail.com>
2023-03-13 22:55:21 -0600

    added readme

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const eqArrays = require('./eqArrays');
-const assertEqual = require('./assertEqual');
 
 module.exports = {
   head,
   tail,
   middle,
-  assertArraysEqual,
-  eqArrays,
-  assertEqual
 };
